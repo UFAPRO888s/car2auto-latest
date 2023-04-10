17016f294fbcd2bfc44335b735b955a06fadc8d7
beyourbabe60 <beyourbabe60@gmail.com>
2023-04-11 00:50:34 +0700

    Update index.jsx

--- src/pages/add-cars/index.jsx
+++ src/pages/add-cars/index.jsx
@@ -952,7 +952,7 @@ export default function AddCars() {
                   }
                 />
               </div>
-              <div className="relative col-span-2 rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-indigo-600 focus-within:ring-1 focus-within:ring-indigo-600">
+              <div className="relative col-span-2 rounded-md border border-gray-300 px-3 py-2 shadow-sm">
                 <RadioGroup
                   value={selectedTransmission}
                   onChange={setSelectedTransmission}
