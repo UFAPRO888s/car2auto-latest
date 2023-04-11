03b43d9d22459fabf6b34f642f5b4250caea4d40
beyourbabe60 <beyourbabe60@gmail.com>
2023-04-11 12:05:07 +0700

    Update index.jsx

--- src/pages/add-cars/index.jsx
+++ src/pages/add-cars/index.jsx
@@ -708,7 +708,7 @@ export default function AddCars() {
                   onChange={(event) => setAdd_engineNumber(event.target.value)}
                 />
               </div>
-              <div className="relative rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-indigo-600 focus-within:ring-1 focus-within:ring-indigo-600">
+              <div className="relative col-span-2 rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-indigo-600 focus-within:ring-1 focus-within:ring-indigo-600">
                 <label
                   htmlFor="chasisNumber"
                   className="absolute -top-2 left-2 -mt-px inline-block bg-white px-1 text-xs font-medium text-gray-900"
@@ -720,7 +720,7 @@ export default function AddCars() {
                   name="chasisNumber"
                   id="chasisNumber"
                   value={Add_chasisNumber}
-                  className="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
+                  className="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 md:text-sm"
                   placeholder="เลขตัวถัง"
                   onChange={(event) => setAdd_chasisNumber(event.target.value)}
                 />
@@ -915,23 +915,7 @@ export default function AddCars() {
                   onChange={(event) => setAdd_engineCc(event.target.value)}
                 />
               </div>
-              <div className="relative col-span-2 rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-indigo-600 focus-within:ring-1 focus-within:ring-indigo-600">
-                <label
-                  htmlFor="bodyType"
-                  className="absolute -top-2 left-2 -mt-px inline-block bg-white px-1 text-xs font-medium text-gray-900"
-                >
-                  ประเภทรถยนต์
-                </label>
-                <input
-                  type="text"
-                  name="bodyType"
-                  id="bodyType"
-                  value={Add_bodyType}
-                  className="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 md:text-sm"
-                  //placeholder="LINE ID"
-                  onChange={(event) => setAdd_bodyType(event.target.value)}
-                />
-              </div>
+              
             </div>
             {/* ing */}
 
