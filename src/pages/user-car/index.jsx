9e722f2aa1d144ddf95926af2a2cd8bcc5ad61da
beyourbabe60 <beyourbabe60@gmail.com>
2023-04-12 12:49:45 +0700

    ฟหห

--- src/pages/user-car/index.jsx
+++ src/pages/user-car/index.jsx
@@ -3,9 +3,52 @@ import { SwiperSlideX } from '@/components/SwiperSlideX'
 
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
+import { Container } from '@/components/Container'
+import Iframe from 'react-iframe'
+import { useEffect, useState } from 'react'
+import Image from 'next/image'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
+import { Combobox } from '@headlessui/react'
 
+import { BrandData } from '@/data/brand'
+import original from '@/data/OptCars'
+import Yearvalue from '@/data/year'
+import Select_Image from '@/components/Select_Image'
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
 export default function UserMag() {
+  const [query, setQuery] = useState('')
+  const [selectedBrandData, setSelectedBrandData] = useState('')
+  const [selectedModelData, setSelectedModelData] = useState('')
+  const [selectedYearData, setSelectedYearData] = useState('')
+
+  const filteredYearData =
+    query === ''
+      ? Yearvalue
+      : Yearvalue.filter((Year) => {
+          return Year.YearName.toLowerCase().includes(query.toLowerCase())
+        })
+
+  const filteredBrandData =
+    query === ''
+      ? BrandData
+      : BrandData.filter((Brand) => {
+          //  console.log(Brand[selectedYearData?.YearCode])
+          return Brand.name.toLowerCase().includes(query.toLowerCase())
+        })
+
+  const filteredModelData =
+    query === ''
+      ? BrandData
+      : BrandData.filter((Brand) => {
+          return Brand.name.toLowerCase().includes(query.toLowerCase())
+        })
+  //console.log(filteredBrandData[0][selectedYearData?.YearCode])
+  //console.log(selectedBrandData['name'].toLowerCase())
+
   return (
     <>
       <Head>
@@ -20,8 +63,22 @@ export default function UserMag() {
       <Header />
       <SwiperSlideX />
       <main>
-      
-
+        <Container>
+          <div className="py-8 hidden md:flex">
+            <Iframe
+              url="https://sale.car2autobuy.com/cars-add-new/"
+              width="100%"
+              height="320px"
+              id="add_car"
+              className="aspect-auto w-full h-[100rem]"
+              display="block"
+              position="relative"
+            />
+          </div>
+          <div className='py-8 flex md:hidden'>
+            <h2>ยังไม่รองรับมือถือ กรุณาใช้ในคอมพิวเตอร์</h2>
+          </div>
+        </Container>
       </main>
       <Footer />
     </>
